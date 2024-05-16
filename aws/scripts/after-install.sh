061f331de9607bd03a70b1c0de0e3e199df7aa4c
Vishnu Vardhan Reddy Adena <96369799+vishnu-adena@users.noreply.github.com>
2024-05-16 18:54:52 +0530

    Update after-install.sh

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://edhukanproductservice11-webappdeploymentbucket-esflysr6jjfj/SpringBootHelloWorldExampleApplication.war /usr/local/tomcat9/webapps/SpringBootHelloWorldExampleApplication.war
+aws s3 cp s3://edhukan-webappdeploymentbucket-psfaoywphuno/ProductService-v-0.0.2.jar /usr/local/tomcat9/webapps/ProductService-v-0.0.2.jar
 
 
 # Ensure the ownership permissions are correct.
